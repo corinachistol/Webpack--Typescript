767d7008bea2bd8b5ffecbd3bce232e5604009b9
corinachistol <chistolcorina19@gmail.com>
2023-10-10 14:36:15 +0300

    webpack dev server error

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default {
+    mode: 'development',
     entry: './src/index.ts',
     module: {
         rules: [
@@ -16,6 +17,7 @@ export default {
         ]
     },
     output:{
+        publicPath: 'public',
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public') // pentru a obtine absolute path to the  folder public,where will be bundled all the files together
     }
